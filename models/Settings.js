548c8868e455bf48c5b0fbeadbf86d72272b7f52
itzmahi3063 <mahi01715412@gmail.com>
2026-09-14 07:57:15 +0600

    Delete models directory

--- models/Settings.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const SettingsSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true }, // e.g. "diamondToUsdtRate"
-  value: mongoose.Schema.Types.Mixed,
-});
-
-export default mongoose.models.Settings || mongoose.model("Settings", SettingsSchema);
